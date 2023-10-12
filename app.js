8828b707b5021d355e22c41def03dbf25e9deae6
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-10-12 10:46:58 +0200

    primo esperimento in ts

---
+++ app.js
@@ -0,0 +1,4 @@
+function somma(a, b) {
+    return a + b;
+}
+console.log(somma(3, 5)); //avendo identificato le variabili come numeri, typescript mi correggerà direttamente l'errore qui
---
+++ app.ts
@@ -0,0 +1,5 @@
+function somma(a: number ,b: number){
+   return a+b
+}
+
+console.log(somma(3, 5)) //avendo identificato le variabili come numeri, typescript mi correggerà direttamente l'errore qui
